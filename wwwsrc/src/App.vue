85f70340f8750e0ccc328b56d10d2eda17e87da8
tompasini <pasinitom91@gmail.com>
2021-01-24 13:25:45 -0700

    removed codeworks refs, nav items, and about pg

--- wwwsrc/src/App.vue
+++ wwwsrc/src/App.vue
@@ -11,7 +11,7 @@
   </main>
   <footer>
     <div class="bg-dark text-light text-center p-4">
-      Made with 💖 by CodeWorks
+      Keepr
     </div>
   </footer>
 </template>
--- wwwsrc/src/components/navbar.vue
+++ wwwsrc/src/components/navbar.vue
@@ -1,14 +1,5 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <router-link class="navbar-brand d-flex" :to="{ name: 'Home' }">
-      <div class="d-flex flex-column align-items-center">
-        <img
-          alt="logo"
-          src="../assets/img/cw-logo.png"
-          height="45"
-        />
-      </div>
-    </router-link>
     <button
       class="navbar-toggler"
       type="button"
@@ -25,11 +16,6 @@
         <li class="nav-item">
           <router-link :to="{ name: 'Home' }" class="nav-link">
             Home
-          </router-link>
-        </li>
-        <li class="nav-item">
-          <router-link :to="{ name: 'About' }" class="nav-link">
-            About
           </router-link>
         </li>
       </ul>
